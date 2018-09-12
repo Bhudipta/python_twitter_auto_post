e1e0979b4e472da708cff3644e8163adf1ded734
bhudipta <bhudipta@gmail.com>
2018-09-12 11:46:04 +0600

    first commit

---
+++ bot.py
@@ -0,0 +1,23 @@
+#necessiry libs
+
+import tweepy as tp
+import time
+import os
+
+#credentials to login to twitter api
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_secret = ''
+
+#twitter account api
+auth = tp.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+api = tp.API(auth)
+
+os.chdir('models')
+
+#iterates over pictures in folder
+for model_image in os.listdir('.'):
+    api.update_with_media(model_image)
+    time.sleep(3)
